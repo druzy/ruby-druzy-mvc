2c02ec6d699dcf90e1ed6fe6beb154462ece9ad3
druzy <jonathan.legreneur@free.fr>
2016-05-30 21:32:24 +0200

    release 1.1.0

--- lib/druzy/mvc/mvc_with_result.rb
+++ lib/druzy/mvc/mvc_with_result.rb
@@ -1,15 +1,38 @@
 module Druzy
   module MVC
+    
     class ControllerWithResult < Druzy::MVC::Controller
       def initialize(model)
         super(model)
       end
+      
+      def result
+        if @model.result == nil
+          @model.stopped_thread=Thread.current
+          Thread.stop
+        end
+        return @model.result
+      end
+      
     end
     
     class ModelWithResult < Druzy::MVC::Model
+      attr_accessor :result, :stopped_thread
+      
       def initialize
         super()
+        
+        @result = nil
+        @stopped_thread = nil
+      end
+      
+      def result=(result)
+          @result = result
+          if @stopped_thread != nil
+            @stopped_thread.wakeup
+          end
       end
     end
+    
   end
 end
